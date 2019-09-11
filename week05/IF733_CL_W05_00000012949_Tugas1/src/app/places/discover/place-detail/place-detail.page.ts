3eb0560af0dde7f024b735d3c924bcf28a4c744f
ezra.hayvito <ezra.hayvito@gmail.com>
2019-09-11 12:07:25 +0700

    [week05] Book Place

---
+++ week05/IF733_CL_W05_00000012949_Tugas1/src/app/places/discover/place-detail/place-detail.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, ModalController } from '@ionic/angular';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+
+@Component({
+    selector: 'app-place-detail',
+    templateUrl: './place-detail.page.html',
+    styleUrls: ['./place-detail.page.scss'],
+})
+export class PlaceDetailPage implements OnInit {
+    place: Place;
+
+    constructor(
+        private route: ActivatedRoute,
+        private navCtrl: NavController,
+        private placesService: PlacesService,
+        private modalCtrl: ModalController
+    ) { }
+
+    ngOnInit() {
+        this.route.paramMap.subscribe(paramMap => {
+            if (!paramMap.has('placeId')) {
+                this.navCtrl.navigateBack('/places/tabs/offers');
+                return;
+            }
+            this.place = this.placesService.getPlace(paramMap.get('placeId'));
+        })
+    }
+
+    onBookPlace() {
+        this.modalCtrl
+            .create({
+                component: CreateBookingComponent,
+                componentProps: { selectedPlace: this.place }
+            })
+            .then(modalElement => {
+                modalElement.present();
+                return modalElement.onDidDismiss();
+            })
+            .then(resultData => {
+                console.log(resultData.data, resultData.role);
+                if (resultData.role === 'confirm') {
+                    console.log('BOOKED')
+                }
+            });
+    }
+}
